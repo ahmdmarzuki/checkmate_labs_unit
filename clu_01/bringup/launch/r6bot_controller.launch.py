87f2c857a9a9ff4830697711195063f86a3e1742
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2026-02-17 23:35:21 +0700

    refactor: robot name

--- clu_01/bringup/launch/r6bot_controller.launch.py
+++ clu_01/bringup/launch/r6bot_controller.launch.py
@@ -13,9 +13,11 @@
 # limitations under the License.
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import RegisterEventHandler, DeclareLaunchArgument
 from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
@@ -27,13 +29,9 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "gui",
             default_value="true",
-            description="Start Rviz2 and Joint State Publisher gui automatically \
-        with this launch file.",
+            description="Start RViz2 automatically with this launch file.",
         )
     )
-    # Initialize Arguments
-    gui = LaunchConfiguration("gui")
-
     # Get URDF via xacro
     robot_description_content = Command(
         [
@@ -50,22 +48,34 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
 
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("clu_01"),
+            "config",
+            "r6bot_controller.yaml",
+        ]
+    )
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare("ros2_control_description"), "r6bot/rviz", "view_robot.rviz"]
     )
 
-    joint_state_publisher_node = Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui",
-        condition=IfCondition(gui),
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+        ],
+        output="both",
     )
-
-    robot_state_publisher_node = Node(
+    robot_state_pub_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
         parameters=[robot_description],
     )
+
+    gui = LaunchConfiguration("gui")
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
@@ -74,11 +84,41 @@ def generate_launch_description():
         arguments=["-d", rviz_config_file],
         condition=IfCondition(gui),
     )
-    #
-    nodes_to_start = [
-        joint_state_publisher_node,
-        robot_state_publisher_node,
-        rviz_node,
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["r6bot_controller", "-c", "/controller_manager"],
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        )
+    )
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+    nodes = [
+        control_node,
+        robot_state_pub_node,
+        joint_state_broadcaster_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
     ]
 
-    return LaunchDescription(declared_arguments + nodes_to_start)
+    return LaunchDescription(declared_arguments + nodes)
